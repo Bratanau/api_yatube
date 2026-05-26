d38a5edb51f6587b923827a0c28d8ce64d8aab8e
Ilya <bratanau@icloud.com>
2026-05-26 20:15:27 +0300

    aaa

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -9,7 +9,7 @@ from .permissions import IsAuthorOrReadOnly
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticated, IsAuthorOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -23,7 +23,7 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticated, IsAuthorOrReadOnly)
 
     def get_post(self):
         post_id = self.kwargs.get('post_id')
